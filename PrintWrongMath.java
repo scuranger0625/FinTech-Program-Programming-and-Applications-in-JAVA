30f9dff969428d1b6cf89b77c562eeaa72323ff7
Leon <166517099+scuranger0625@users.noreply.github.com>
2025-03-30 00:07:59 +0800

    Add files via upload

---
+++ PrintWrongMath.java
@@ -0,0 +1,7 @@
+/* 2_3. 輸出語句 - 不是計算 (20.0) */
+public class PrintWrongMath {
+    public static void main(String[] args) {
+        // 直接印出 "2-3=5" 作為字串，不進行計算
+        System.out.println("2-3=5");
+    }
+}
