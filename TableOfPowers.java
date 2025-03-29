fc2243829839e2833db0a175780766e1c0da2b1b
Leon <166517099+scuranger0625@users.noreply.github.com>
2025-03-30 01:49:21 +0800

    TableOfPowers.java

---
+++ TableOfPowers.java
@@ -0,0 +1,17 @@
+/*2_4.輸出語句-多列資料(20.0)*/
+public class TableOfPowers {
+    
+    public static void main(String[] args){
+        System.out.println("1 1 1");
+        System.out.println("2 4 8");
+        System.out.println("3 9 27");
+        System.out.println("4 16 64");
+        System.out.println("5 25 125");
+        System.out.println("6 36 216 ");
+        System.out.println("7 49 343");
+        System.out.println("8 64 512");
+        System.out.println("9 81 729");
+        System.out.println("10 100 1000");
+    }
+     
+}
