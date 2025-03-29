3560f65f25ab8f74d650490c4a4a05fba6bcc71c
Leon <166517099+scuranger0625@users.noreply.github.com>
2025-03-29 23:38:14 +0800

    Create 2_2. 輸出語句 - 印三角形.java

---
+++ 2_2. 輸出語句 - 印三角形.java
@@ -0,0 +1,22 @@
+/* 2_2. 輸出語句 - 印三角形 (20.0) */
+/*寫一個程式，印出如輸出範例的結果：
+繼續使用print的指令，只是需要執行6次，然後印出六行不同數量的星星。
+
+輸出範例:
+*
+**
+***
+****
+*****
+*******/
+public class Hello {
+    public static void main(String[] args) {
+        // 直接印出星星，使用 println 一次印一行
+        System.out.println("*");      // 第1行
+        System.out.println("**");     // 第2行
+        System.out.println("***");    // 第3行
+        System.out.println("****");   // 第4行
+        System.out.println("*****");  // 第5行
+        System.out.println("******"); // 第6行
+    }
+}
